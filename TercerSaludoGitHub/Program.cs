0c569b6da8014c63d1a172f473d72168441bd217
MaxYbrNotebook <MaxYbrNotebook@DESKTOP-83HACU0>
2024-09-23 23:43:55 -0300

    Agregar archivos de proyecto.

---
+++ TercerSaludoGitHub/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TercerSaludoGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tercer Saludo para GitHub!!");
+            Console.ReadKey();
+        }
+    }
+}
